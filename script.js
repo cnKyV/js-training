9b46a33ecef4983756fffbb7c55d84f0449a14e3
cnKyV <cenkay_vergili4@hotmail.com>
2020-10-28 11:53:48 +0300

    first

---
+++ script.js
@@ -0,0 +1,31 @@
+/*
+
+
+if(true)
+    {
+        console.log(a);
+        var a = 500;
+        console.log(a);
+    }*/
+
+/*function sendCars(a,...carIds)
+{
+    carIds.forEach(id=>console.log(id));
+}
+
+sendCars("Monday",3,4,5,6,7); */
+
+/*let carIds = [1,2,5];
+let car1, remainingCars;
+[car1,...remainingCars] = carIds;
+console.log(remainingCars);
+*/
+/*
+let carIds = [100,200,500];
+let [car1,car2] =carIds;
+console.log(car1,car2);
+*/
+
+let car ={id:5000, style:"convertible"};
+
+
