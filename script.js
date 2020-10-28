bf9a804b45a33654ff97b1dac0b8de26fcfd7604
cnKyV <cenkay_vergili4@hotmail.com>
2020-10-29 01:42:51 +0300

    kekw

--- script.js
+++ script.js
@@ -19,14 +19,14 @@ sendCars("Monday",3,4,5,6,7); */
 let car1, remainingCars;
 [car1,...remainingCars] = carIds;
 console.log(remainingCars);
-*/
-/*
+
+
 let carIds = [100,200,500];
 let [car1,car2] =carIds;
 console.log(car1,car2);
-*/
 
-/*
+
+
 let car ={id:5000, style:"convertible"};
 let car2 = car;
 console.log(car2);
@@ -37,3 +37,37 @@ let id, style, type;
 console.log(id, style, type);
 */
 
+/*function startCars(car1,car2,car3)
+{
+    console.log(car1,car2,car3);
+}
+let carIds = [1,2,3,4,5,6];
+startCars(...carIds);
+*/
+//convert to string
+/*
+let foo = 55;
+console.log(typeof(foo.toString()));
+console.log(typeof(Number.parseInt("653aa")));
+console.log(typeof(Number.parseFloat("4433.22bb")));
+*/
+
+for(let i = 0; i<4; i++)
+{
+    if(i===2)
+    {
+        continue;
+    }
+    console.log(i);
+} // output: 0 1 3
+
+let i = 0;
+
+for (; i <12 ; i++) 
+{
+if(i === 8)
+{
+    break;
+}
+}
+console.log(i); // 8
